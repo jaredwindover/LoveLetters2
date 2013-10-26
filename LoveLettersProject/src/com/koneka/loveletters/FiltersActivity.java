4ea82b1893381836f288c726e1b1e48fc1ef4c04
fedor_000 <fedor_000@Gavin-laptop>
2013-10-26 14:19:56 -0400

    shit is getting done

--- LoveLettersProject/src/com/koneka/loveletters/FiltersActivity.java
+++ LoveLettersProject/src/com/koneka/loveletters/FiltersActivity.java
@@ -2,7 +2,11 @@ package com.koneka.loveletters;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FiltersActivity extends Activity {
 
@@ -10,6 +14,19 @@ public class FiltersActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_filter);
+		
+		final Intent intent = new Intent(this, FieldsActivity.class);
+		final Button button = (Button) findViewById(R.id.filterNextBtn);
+		
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				try{					
+					startActivity(intent);
+				}
+				catch(Exception e){			
+				}
+			}
+		});
 	}
 
 	@Override
