d00a0fa9a4b4d00272094802508183ac18ba620c
fedor_000 <fedor_000@Gavin-laptop>
2013-10-26 15:37:30 -0400

    shit is getting connected

--- LoveLettersProject/src/com/koneka/loveletters/FieldsActivity.java
+++ LoveLettersProject/src/com/koneka/loveletters/FieldsActivity.java
@@ -4,8 +4,13 @@ import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class FieldsActivity extends Activity {
 
@@ -14,19 +19,54 @@ public class FieldsActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fields);
 		
+		final Intent intent = new Intent(this, FieldsActivity.class);
+		final Button button = (Button) findViewById(R.id.filterNextBtn);
+			
 		Bundle b = getIntent().getBundleExtra("jsonBundle");
-		String jsonString = b.getString("jsonString");
-		Log.d("test", jsonString);
-		/*
-		try
-		{
-		JSONObject json = new JSONObject(jsonString);
-		}
-		catch(Exception e){}
-		*/
+		
+		final String jsonString = b.getString("jsonString");
+		final JSONObject fieldParams = new JSONObject();
+		Log.d("test-fields", jsonString);
+		
 		// ADD VALUES FROM UI TO JSON HERE
 
 		// ADD BUNDLE AND START NEW INTENT
+		
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				
+				EditText firstName = (EditText) findViewById(R.id.firstText);
+				EditText lastName = (EditText) findViewById(R.id.lastText);
+				EditText rest = (EditText) findViewById(R.id.restaurantText);
+				EditText park = (EditText) findViewById(R.id.parkText);
+				EditText theater = (EditText) findViewById(R.id.theaterText);
+				EditText time = (EditText) findViewById(R.id.timeText);
+				EditText location = (EditText) findViewById(R.id.locationText);
+				try
+				{
+					JSONObject json = new JSONObject(jsonString);
+					
+					fieldParams.put("FIRSTNAME", firstName.getText() );
+					fieldParams.put("LASTNAME", lastName.getText() );
+					fieldParams.put("RESTAURANTNAME", rest.getText() );
+					fieldParams.put("PARKNAME", park.getText() );
+					fieldParams.put("THEATERNAME", theater.getText() );
+					fieldParams.put("TIME", time.getText() );
+					fieldParams.put("LOCATION", location.getText() );
+				
+					json.put("fields", fieldParams);
+					
+					Bundle b2 = new Bundle();
+					b2.putString("jsonString", json.toString());
+					
+					intent.putExtra("jsonBundle", b2);
+					Log.d("test", json.toString());
+					startActivity(intent);
+				}
+				catch(Exception e){			
+				}
+			}
+		});
 	}
 
 	@Override
